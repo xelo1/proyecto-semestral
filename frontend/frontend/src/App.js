7f851ffa40506611c35e7d5e89eb0fa023c3a9c8
Cuchuflito <111323461+Cuchuflito@users.noreply.github.com>
2023-12-20 03:18:40 -0300

    crud materiales

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,24 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Inventario from './scenes/materiales/material_crud';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Your App</h1>
+      </header>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/inventario">Inventario</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/inventario" element={<Inventario />} />
+          {/* Add more routes as needed */}
+          {/* <Route path="/another-page" element={<AnotherPageComponent />} /> */}
+          {/* ... */}
+          {/* If you want a default route for unmatched paths */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
