250428048238739ae9d8f95ffe2cb6d6a2a771ba
Cuchuflito <111323461+Cuchuflito@users.noreply.github.com>
2023-12-20 21:35:56 -0300

    Arreglo home
    si

--- frontend/frontend/src/scenes/home.jsx
+++ frontend/frontend/src/scenes/home.jsx
@@ -17,15 +17,13 @@ function Home() {
       </Box>
 
       <Typography variant="body1" sx={{ mt: 2 }}>
-        Somos una empresa líder en la producción y comercialización de cementos en Chile. Con más de 50 años de experiencia, nos enfocamos en brindar productos y servicios de la más alta calidad a nuestros clientes.
-      </Typography>
-
+        Somos una empresa que  vende cemento.
+        </Typography>
       <Typography variant="body1">
-        Nuestras modernas plantas de producción y nuestro equipo de profesionales garantizan la excelencia de todos nuestros productos. Usamos las más avanzadas tecnologías y seguimos rigurosos controles de calidad.
+      Nuestra función, es vender cemento.
       </Typography>
-
       <Typography variant="body1" sx={{ mt: 2 }}>
-        Agradecemos su preferencia por nuestros cementos. Estamos para servirle.
+        ¿Quiere comprar cemento?
       </Typography>
     </Box>
   );
